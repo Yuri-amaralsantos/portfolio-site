fa7d2b180c559211a65722e52bd43c9d40daf868
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-08-02 22:37:10 -0300

    refactor: move footer texto to right

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,12 @@ import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white mt-12">
-      <div className="container mx-auto px-4 py-10 flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
-        <div className="text-center md:text-left">
-          <a href="#" className="text-3xl font-bold hover:text-blue-400">
-            Yuri Amaral Santos
-          </a>
-
+    <footer id="footer" className="bg-[#0a0c0f] text-white mt-12">
+      <div className="container mx-auto px-12 py-10 flex flex-col md:flex-row justify-between items-center gap-6">
+        <a href="#" className="text-3xl font-bold hover:text-blue-400">
+          Yuri Amaral Santos
+        </a>
+        <div className="text-center md:text-right">
           <div className="mt-4 space-y-1">
             <h3>
               <a
@@ -28,7 +27,7 @@ const Footer = () => {
             </h3>
           </div>
 
-          <div className="mt-6 flex justify-center md:justify-start space-x-6 text-gray-300">
+          <div className="mt-6 flex justify-center md:justify-end space-x-6 text-gray-300">
             <a
               href="https://www.linkedin.com/in/yuri-amaral-santos-17264a25b/"
               target="_blank"
