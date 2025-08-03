03dcfded7469fcfe80fbae802d3785a39b2ac575
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-08-02 23:51:53 -0300

    feat: change logic for arriows appear only in web and all recents projects appear in mobile

--- src/sections/ProjectSection.jsx
+++ src/sections/ProjectSection.jsx
@@ -1,42 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ProjectCard from "../components/ProjectCard";
 import projects from "../data/projects.json";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const ProjectsSection = () => {
-  const [page, setPage] = useState(0);
-  const projectsPerPage = 3;
   const maxProjects = projects.slice(0, 6);
+  const [isMobile, setIsMobile] = useState(false);
+  const [page, setPage] = useState(0);
+
+  const projectsPerPage = 3;
   const totalPages = Math.ceil(maxProjects.length / projectsPerPage);
+
+  useEffect(() => {
+    const checkMobile = () => setIsMobile(window.innerWidth < 768);
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
   const handlePrev = () => setPage((prev) => Math.max(prev - 1, 0));
   const handleNext = () =>
     setPage((prev) => Math.min(prev + 1, totalPages - 1));
 
-  const currentProjects = maxProjects.slice(
-    page * projectsPerPage,
-    (page + 1) * projectsPerPage
-  );
+  const displayedProjects = isMobile
+    ? maxProjects
+    : maxProjects.slice(page * projectsPerPage, (page + 1) * projectsPerPage);
 
   return (
-    <section className="py-6 px-6 relative text-black">
+    <section className="py-6 px-1 md:px-6 relative text-black">
       <h2 className="text-2xl font-bold mb-4 text-center">Projetos</h2>
 
       <div className="relative max-w-5xl mx-auto">
-        {page > 0 && (
+        {!isMobile && page > 0 && (
           <button
             onClick={handlePrev}
-            className="absolute -left-24 top-1/2 -translate-y-1/2 z-10 bg-gray-400 text-black p-3 rounded-full shadow hover:bg-gray-700"
+            className="absolute -left-24 top-1/2 -translate-y-1/2 z-10 bg-black text-white p-3 rounded-full shadow hover:bg-white hover:text-black"
             aria-label="Anterior"
           >
             <FaChevronLeft size={20} />
           </button>
         )}
-        {page < totalPages - 1 && (
+        {!isMobile && page < totalPages - 1 && (
           <button
             onClick={handleNext}
-            className="absolute -right-24 top-1/2 -translate-y-1/2 z-10 bg-gray-400 text-black p-3 rounded-full shadow hover:bg-gray-700"
+            className="absolute -right-24 top-1/2 -translate-y-1/2 z-10 bg-black text-white p-3 rounded-full shadow hover:bg-white hover:text-black"
             aria-label="Próximo"
           >
             <FaChevronRight size={20} />
@@ -44,7 +52,7 @@ const ProjectsSection = () => {
         )}
 
         <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center">
-          {currentProjects.map((project, index) => (
+          {displayedProjects.map((project, index) => (
             <ProjectCard
               key={index}
               title={project.title}
@@ -57,10 +65,10 @@ const ProjectsSection = () => {
         </div>
       </div>
 
-      <div className="mt-6 text-center ">
+      <div className="mt-6 text-center">
         <Link
           to="/projects"
-          className="inline-block px-6 py-2 border hover:border-black bg-black text-white  hover:bg-white hover:text-black rounded transition"
+          className="inline-block px-6 py-2 font-bold rounded border border-black bg-black text-white hover:bg-white hover:text-black transition"
         >
           Ver mais projetos
         </Link>
