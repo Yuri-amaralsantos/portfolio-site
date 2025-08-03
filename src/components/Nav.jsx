250534d8390a0ea1ee2b8add8a6657d2cd9cae1c
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-08-02 23:22:42 -0300

    refactor: add contacts in navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const [menuVisible, setMenuVisible] = useState(false);
-  const location = useLocation();
 
   const toggleMenu = () => setMenuVisible((prev) => !prev);
 
-  const pageLinks = [{ path: "/projects", label: "Projetos" }];
+  const pageLinks = [
+    { path: "/", label: "Inicio" },
+    {
+      path: "/projects",
+      label: "Projetos",
+    },
+  ];
 
   const renderLinks = (isMobile = false) => (
     <>
@@ -23,39 +28,38 @@ const Nav = () => {
           {label}
         </Link>
       ))}
-      <a
-        href={`${location.pathname}#footer`}
-        className={`block px-4 py-2 hover:text-blue-500 ${
-          !isMobile ? "inline-block mr-6" : "mb-2"
-        }`}
-        onClick={() => isMobile && setMenuVisible(false)}
-      >
-        Contato
-      </a>
     </>
   );
 
   return (
-    <header className="bg-gray-900 text-white shadow-md fixed w-full z-50">
-      <div className="container mx-auto flex justify-between items-center px-4 py-3">
+    <header className="bg-[#0a0c0f] text-white shadow-md fixed w-full z-50">
+      <div className="w-full max-w-7xl mx-auto flex justify-between items-center px-4 py-3">
         <Link to="/" className="text-2xl font-bold">
           Yuri Amaral Santos
         </Link>
 
         <nav className="hidden md:flex items-center">
-          {renderLinks()}
-          <a
-            href="curriculoti.pdf"
-            download
-            className="inline-block bg-white text-gray-900 px-4 py-2 rounded hover:bg-gray-200 font-semibold"
-          >
-            Currículo
-          </a>
+          <nav className="hidden md:flex items-center">
+            {renderLinks()}
+            <a
+              href="#footer"
+              className="inline-block px-4 py-2 hover:text-blue-500 mr-6"
+            >
+              Contato
+            </a>
+            <a
+              href="curriculoti.pdf"
+              download
+              className="inline-block rounded bg-white text-black px-4 py-2 border border-white hover:bg-black hover:text-white font-semibold"
+            >
+              Currículo
+            </a>
+          </nav>
         </nav>
 
         <button
           onClick={toggleMenu}
-          className="md:hidden focus:outline-none text-3xl"
+          className="block md:hidden focus:outline-none text-3xl"
           aria-label="Toggle menu"
         >
           ☰
@@ -63,19 +67,19 @@ const Nav = () => {
       </div>
 
       {menuVisible && (
-        <div className="md:hidden bg-gray-800 text-white px-4 pb-4 space-y-2 shadow-inner">
-          <Link
-            to="/"
+        <div className="md:hidden bg-black text-white px-4 pb-4 space-y-2 shadow-inner max-h-screen overflow-y-auto">
+          {renderLinks(true)}
+          <a
+            href="#footer"
             className="block px-4 py-2 hover:text-blue-400"
             onClick={() => setMenuVisible(false)}
           >
-            Home
-          </Link>
-          {renderLinks(true)}
+            Contato
+          </a>
           <a
             href="curriculoti.pdf"
             download
-            className="block px-4 py-2 bg-white text-gray-900 rounded hover:bg-gray-200 font-semibold"
+            className="block rounded px-4 py-2 bg-white text-black font-semibold"
             onClick={() => setMenuVisible(false)}
           >
             Currículo
